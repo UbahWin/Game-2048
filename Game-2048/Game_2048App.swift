c969eaf4460c26306c72905a33303efc055cce6d
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-05-07 22:35:47 +0300

    Initial Commit

---
+++ Game-2048/Game_2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Game_2048App.swift
+//  Game-2048
+//
+//  Created by Иван Вдовин on 07.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Game_2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
