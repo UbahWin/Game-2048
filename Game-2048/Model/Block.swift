2baac5857d266a1118e60bd970fbecfa8a606aed
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-05-21 19:03:43 +0300

    change: colours

--- Game-2048/Model/Block.swift
+++ Game-2048/Model/Block.swift
@@ -34,17 +34,17 @@ class Block: Equatable {
             _value = newValue
             switch newValue {
             case 0: color = .gray
-            case 2: color = Color(hex: 0xf2e8c9)
-            case 4: color = Color(hex: 0xffe4c4)
-            case 8: color = Color(hex: 0xfae7b5)
-            case 16: color = Color(hex: 0xffdb8b)
-            case 32: color = Color(hex: 0xfce883)
-            case 64: color = Color(hex: 0xfddb6d)
-            case 128: color = Color(hex: 0xffcf48)
-            case 256: color = Color(hex: 0xefa94a)
-            case 512: color = Color(hex: 0xffb28b)
-            case 1024: color = Color(hex: 0xff8c69)
-            case 2048: color = Color(hex: 0xe4717a)
+            case 2: color = Color(hex: 0xE7C697)
+            case 4: color = Color(hex: 0xEFA94A)
+            case 8: color = Color(hex: 0x5D9B9B)
+            case 16: color = Color(hex: 0x77DD77)
+            case 32: color = Color(hex: 0xFCE883)
+            case 64: color = Color(hex: 0xDCD0FF)
+            case 128: color = Color(hex: 0xAFDAFC)
+            case 256: color = Color(hex: 0x3EB489)
+            case 512: color = Color(hex: 0xFF8C69)
+            case 1024: color = Color(hex: 0xFF7514)
+            case 2048: color = Color(hex: 0xE4717A)
             default: color = .gray
             }
             sizeValueText = makeSize(value: newValue)
